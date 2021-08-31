3636e4b1bf18734b44c7806779e275036e465127
vagrant <rpg7ofdeath@gmail.com>
2021-08-31 01:48:01 +0000

    Added "Create New URL" page in ./views/ and added generateRandomString function and required body-parser in express_server.js

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   'b2xVn2': 'https://www.lighthouselabs.ca',
@@ -22,9 +24,18 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars)
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
 });
 
 app.get('/hello', (req, res) => {
@@ -34,3 +45,10 @@ app.get('/hello', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+const generateRandomString = () => {
+  randomString = Math.random().toString(36).substring(7);
+  if (urlDatabase.includes(randomString))
+    return generateRandomString;
+  return randomString;
+};
