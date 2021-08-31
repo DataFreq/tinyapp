36be1d4555f728e96574673bbb49e8d1b3fcc0f1
vagrant <rpg7ofdeath@gmail.com>
2021-08-31 23:00:45 +0000

    Implemented edit longURL button, updated express_server accordingly

--- express_server.js
+++ express_server.js
@@ -27,22 +27,31 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
+
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/urls', (req, res) => {
+app.get('/urls', (req, res) => { //urls_index.ejs
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
-app.get('/urls/new', (req, res) => {
+app.get('/urls/new', (req, res) => { // urls_new.ejs
   res.render('urls_new');
 });
 
-app.get('/urls/:shortURL', (req, res) => {
+app.get('/urls/:shortURL', (req, res) => { // urls_show.ejs
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.post('/urls', (req, res) => {
@@ -58,13 +67,16 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect('/urls');
 });
 
-app.get('/u/:shortURL', (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
+app.post('/urls/:shortURL/edit', (req, res) => { // Edit button on urls_index.ejs redirects to urls_show.ejs
+  let shortURL = req.params.shortURL;
+  res.redirect(`/urls/${shortURL}`);
+})
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
+app.post('/urls/:id', (req, res) => {
+  const shortURL = req.params.id;
+  urlDatabase[shortURL] = req.body.newURL;
+  writeToDisk(urlDatabase);
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
