8ed6f680757b42521c8ff6e0e1d37747cfc2c738
vagrant <rpg7ofdeath@gmail.com>
2021-09-02 04:40:07 +0000

    refactored code

--- express_server.js
+++ express_server.js
@@ -14,49 +14,41 @@ let users = '';
 let urlDatabase = '';
 
 fs.readFile('./data/urlDatabase.json', 'utf8', (err, jsonString) => { // initial load of urlDatabase.json
-  if (err) {
-    console.log('File read failed:', err);
-    return;
-  }
+  if (err) return console.log('File read failed:', err);
   urlDatabase = JSON.parse(jsonString);
 });
 
 fs.readFile('./data/userDatabase.json', 'utf8', (err, jsonString) => { // initial load of userDatabase.json
-  if (err) {
-    console.log('File read failed:', err);
-    return;
-  }
+  if (err) return console.log('File read failed:', err);
   users = JSON.parse(jsonString);
 });
 
 app.get('/', (req, res) => {
-  res.redirect('/urls');
+  req.cookies['user_id'] ? res.redirect('/urls') : res.redirect('/login');
 });
 
-app.get('/urls', (req, res) => { //urls_index.ejs
-  if (!req.cookies['user_id']) {
+app.get('/urls', (req, res) => {
+  if (!req.cookies['user_id'])
     return res.redirect('/login');
-  }
-
   const templateVars = {
     urls: pullUserURLs(req.cookies['user_id'], urlDatabase),
     user: users[req.cookies['user_id']],
   };
-
   res.render('urls_index', templateVars);
 });
 
-app.get('/urls/new', (req, res) => { // urls_new.ejs
+app.get('/urls/new', (req, res) => {
+  if (!req.cookies['user_id']) return res.redirect('/login');
   const templateVars = {
     user: users[req.cookies['user_id']],
   };
   res.render('urls_new', templateVars);
 });
 
-app.get('/urls/:shortURL', (req, res) => { // urls_show.ejs
+app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   if (req.cookies['user_id'] !== urlDatabase[shortURL].userID)
-    return res.status(403).send(`Only the owner of the ${shortURL} may edit this shortURL.`);
+    return res.status(403).send(`You do not have permission to edit ${shortURL}.`);
   const templateVars = {
     shortURL: shortURL,
     longURL: urlDatabase[shortURL].longURL,
@@ -86,15 +78,10 @@ app.get('/login', (req, res) => {
 
 /* <-------------------- end of GET -------------------- > */
 
-app.post('/urls/:shortURL/edit', (req, res) => { // Edit button on urls_index.ejs redirects to urls_show.ejs
-  let shortURL = req.params.shortURL;
-  res.redirect(`/urls/${shortURL}`);
-});
-
 app.post('/urls', (req, res) => {
   if (!users[req.cookies['user_id']])
     return res.status(403).send("Only registered accounts may create URLs");
-  let shortURL = generateRandomString(urlDatabase);
+  const shortURL = generateRandomString(urlDatabase);
   urlDatabase[shortURL] = {
     longURL: req.body.longURL,
     userID: req.cookies['user_id']
@@ -103,12 +90,15 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+app.post('/urls/:shortURL/edit', (req, res) => {
+  const shortURL = req.params.shortURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
 app.post('/urls/:shortURL/delete', (req, res) => {
   const shortURL = req.params.shortURL;
-
-  if (!users[req.cookies['user_id']])
+  if (!urlDatabase[shortURL][req.cookies['user_id']])
     return res.status(403).send(`Only the owner of ${shortURL} may delete this shortURL.`);
-
   delete urlDatabase[shortURL];
   writeUrlToDisk(urlDatabase);
   res.redirect('/urls');
@@ -116,10 +106,8 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 
 app.post('/urls/:id', (req, res) => { //edit url
   const shortURL = req.params.id;
-
-  if (!users[req.cookies['user_id']])
-    return res.status(403).send(`Only the owner of ${shortURL} may edit this shortURL.`);
-    
+  if (!urlDatabase[shortURL][req.cookies['user_id']])
+    return res.status(403).send(`You do not have permission to edit ${shortURL}.`);
   urlDatabase[shortURL].longURL = req.body.newURL;
   writeUrlToDisk(urlDatabase);
   res.redirect('/urls');
@@ -129,13 +117,10 @@ app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const id = activeAccount(email, users);
-
   if (!id)
     return res.status(403).send("Account not found");
-
   if (password !== users[id].password)
     return res.status(403).send("Incorrect Password");
-  
   res.cookie('user_id', id);
   res.redirect('/urls');
 });
@@ -149,19 +134,15 @@ app.post('/register', (req, res) => {
   const id = generateRandomString(users);
   const email = req.body.email;
   const password = req.body.password;
-
   if (!email || !password)
     return res.status(404).send("Invalid email or password.");
-
   if (activeAccount(email, users))
     return res.status(404).send("An account with that email already exists.");
-
   users[id] = {
     id: id,
     email: email,
     password : password
   };
-
   writeUserToDisk(users);
   res.cookie('user_id', id);
   res.redirect('/urls');
